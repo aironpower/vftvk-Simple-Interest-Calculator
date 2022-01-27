084f6a3d8e7038b4720ffbba5944a866b8c234e4
Manuel Giron <manuelgironm@gmail.com>
2022-01-27 16:21:07 +0100

    Last Modification

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 function compute()
 {
-    principal = document.getElementById("principal").value;
+    principal = document.getElementById("principal");
     rate = document.getElementById("rate").value;
-    years = parseInt(document.getElementById("year").value);
+    years = parseInt(document.getElementById("years").value);
     result = document.getElementById("result");
     date = new Date().getFullYear()+years;
-    calculate = calculateInterest(principal, years, rate);
+    calculate = principal.value * years * rate / 100;
 
-    text = "If you deposit <span class='yellow'>"+ principal+"</span>,<br>";
-    text += "ata an interest rate of <span class='yellow'>"+ rate+"</span>,<br>";
-    text += "You will receive an amount of <span class='yellow'>"+ calculate +"</span>,<br>";
-    text += "In the year <span class='yellow'>"+date+"</span><br><br>";
-    result.innerHTML = text;
+    if(principal.value <= 0) {
+        alert("Please, enter a positive number");
+        principal.focus();
+    } else {
+        text = "If you deposit <mark>"+ principal.value+"</mark>,<br>";
+        text += "ata an interest rate of <mark>"+ rate+"</mark>,<br>";
+        text += "You will receive an amount of <mark>"+ calculate +"</mark>,<br>";
+        text += "In the year <mark>"+date+"</mark><br><br>";
+        result.innerHTML = text;
+    }
     
 }
 
@@ -19,9 +24,4 @@ function changeRate() {
     rate = document.getElementById("rate").value;
     percent = document.getElementById("percent");
     percent.innerHTML = rate + " %";
-}
-
-function calculateInterest(total,year,rate) {
-    var interest = rate*year/100+1;
-    return Math.round((total*interest)-total);
 }
